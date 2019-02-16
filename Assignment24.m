b6de2e7965f183eccea5768e172be06c9b6ec3e9
YasmineAyman <47699047+YasmineAyman@users.noreply.github.com>
2019-02-16 11:07:18 -0800

    Add files via upload

---
+++ Assignment24.m
@@ -0,0 +1,98 @@
+clc
+clear all
+close all
+ 
+ds = datastore('heart_DD.csv','TreatAsMissing','NA',.....
+    'MissingValue',0,'ReadSize',250);
+T = read(ds);
+m= length(T{:,1});
+p=ceil(0.8*m);
+p1=ceil(0.1*m);
+Alpha=0.01;
+lambda=100;
+ x=T{1:p,1:13};
+ y=T{1:p,14};
+X=[ones(p,1)  x./2 x./4];
+Y=y;
+%Y=T{1:p,14}/mean(T{1:p,14});
+n=length(X(1,:)); 
+ for w=2:n
+    if max(abs(X(:,w)))~=0
+    X(:,w)=(X(:,w)-mean((X(:,w))))./std(X(:,w));
+    end
+ end
+ 
+ theta=zeros(n,1);
+ 
+ h=1./(1+exp(-X*theta));  %sigmoid function
+ 
+ k=1;
+ J(k)=-(1/p)*sum(Y.*log(h)+(1-Y).*log(1-h))+(lambda/(2*p))*sum((theta).^2);  %cost function
+ 
+ grad=zeros(size(theta,1),1);     %gradient vector
+  
+ for i=1:size(grad)
+     grad(i)=(1/p)*sum((h-Y)'*X(:,i));
+ end
+ 
+
+R=1;
+while R==1
+Alpha=Alpha*1;
+theta=theta-(Alpha/p)*X'*(h-Y);
+h=1./(1+exp(-X*theta));  %sigmoid function
+k=k+1
+
+J(k)=(-1/p)*sum(Y.*log(h)+(1-Y).*log(1-h))+(lambda/(2*p))*sum((theta).^2);
+if J(k-1)-J(k) <0 
+    break
+end 
+q=(J(k-1)-J(k))./J(k-1);
+if q <.00001
+    R=0;
+end
+end
+ 
+
+%%%%%%
+
+
+x1=T{p+1:p+p1,1:13};
+y1=T{p+1:p+p1,14};
+X1=[ones(p1,1) x1./2 x1./4];
+%Y1=T{p+1:p+p1,14}/mean(T{p+1:p+p1,14});
+Y1=y1;
+n1=length(X1(1,:)); 
+ for w1=2:n1
+    if max(abs(X1(:,w1)))~=0
+    X1(:,w1)=(X1(:,w1)-mean((X1(:,w1))))./std(X1(:,w1));
+    end
+ end
+ 
+ theta1=theta;
+ h1=1./(1+exp(-X1*theta1));  %sigmoid function
+ 
+ k1=1;
+ J1(k1)=-(1/p1)*sum(Y1.*log(h1)+(1-Y1).*log(1-h1))+(lambda/(2*p1))*sum((theta1).^2);  %cost function
+ 
+ %%%%%%%%
+ 
+x2=T{p+p1+1:end ,1:13};
+y2=T{p+p1+1:end,14};
+X2=[ones(m-(p+p1),1) x2./2 x2./4];
+%Y2=T{p+p1+1:end,14}/mean(T{p+p1+1:end,14});
+Y2=y2;
+n2=length(X2(1,:)); 
+ for w2=2:n1
+    if max(abs(X2(:,w2)))~=0
+    X2(:,w2)=(X2(:,w2)-mean((X2(:,w2))))./std(X2(:,w2));
+    end
+ end
+ 
+ theta2=theta1;
+ h2=1./(1+exp(-X2*theta2));  %sigmoid function
+ 
+ k2=1;
+ J2(k2)=-(1/p1)*sum(Y2.*log(h2)+(1-Y2).*log(1-h2))+(lambda/(2*p1))*sum((theta2).^2);  %cost function
+ 
+ 
---
+++ MachineLearningAssignment14.m
@@ -0,0 +1,93 @@
+clc
+close all
+clear all
+ds = datastore('house_data_complete.csv','TreatAsMissing','NA',.....
+    'MissingValue',0,'ReadSize',25000);
+T = read(ds);
+size(T);
+Alpha=.005;
+m=length(T{:,1});
+p=ceil(0.6*m);
+p1=ceil(0.2*m);
+U0=T{1:p ,2}
+U=T{1:p,4:19};
+U1=T{1:p,20:21};
+U2=U./3;
+U3=U1./2;
+X=[ones(p,1) U U1 U3]
+
+n=length(X(1,:));
+for w=2:n
+    if max(abs(X(:,w)))~=0
+    X(:,w)=(X(:,w)-mean((X(:,w))))./std(X(:,w));
+    end
+end
+
+Y=T{1:p,3}/mean(T{1:p,3});
+Theta=zeros(n,1);
+
+k=1;
+
+E(k)=(1/(2*(p)))*sum((X*Theta-Y).^2);
+
+R=1;
+while R==1
+Alpha=Alpha*1;
+Theta=Theta-(Alpha/p)*X'*(X*Theta-Y);
+k=k+1
+E(k)=(1/(2*(p))*sum((X*Theta-Y).^2));
+if E(k-1)-E(k)<0
+    break
+end 
+q=(E(k-1)-E(k))./E(k-1);
+if q <.0001;
+    R=0;
+end
+end
+plot(E);
+
+%%%%%%%
+
+
+U00=T{p+1:p1+p ,2}
+UU=T{p+1:p1+p,4:19};
+U11=T{p+1:p1+p,20:21};
+U22=UU./3;
+U33=U11./2;
+X1=[ones(p1,1) UU U11 U33]
+Theta1=Theta;
+YY=T{p+1:p1+p,3}/mean(T{p+1:p1+p,3});
+
+k=1;
+n1=length(X1(1,:));
+for w=2:n1
+    if max(abs(X1(:,w)))~=0
+    X1(:,w)=(X1(:,w)-mean((X1(:,w))))./std(X1(:,w));
+    end
+end
+E1(k)=(1/(2*(p1)))*sum((X1*Theta1-YY).^2);
+
+
+%%%%%%%
+
+U000=T{p+p1+1:end ,2}
+UUU=T{p+p1+1:end,4:19};
+U111=T{p+p1+1:end,20:21};
+U222=UUU./3;
+U333=U111./2;
+X2=[ones(length(UUU),1) UUU U111 U333]
+Theta2=Theta1;
+YYY=T{p+p1+1:end,3}/mean(T{p+p1+1:end,3});
+
+k=1;
+n2=length(X2(1,:));
+for w=2:n2
+    if max(abs(X2(:,w)))~=0
+    X2(:,w)=(X2(:,w)-mean((X2(:,w))))./std(X2(:,w));
+    end
+end
+E2(k)=(1/(2*(p1)))*sum((X2*Theta2-YYY).^2);
+
+
+
+
